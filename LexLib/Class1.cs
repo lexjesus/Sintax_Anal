5bfcf29486ce4413aa031fa071c390e3a46c2161
lexjesus <baranovbik@yandex.ru>
2020-12-15 21:14:17 +0300

    Add project files.

---
+++ LexLib/Class1.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+
+namespace lex_analyzer
+{
+
+    enum States { Start, ID, ERROR };
+    public enum Token { KeyWord, Separator, ID, ERROR };
+    public class Lexems
+    {
+       
+
+        public class LexemToken
+        {
+            public string lexem;
+            public Token token;
+            public int cLine;
+            public LexemToken Next;
+
+            public LexemToken(string lexem, Token token, int cLine)
+            {
+                this.lexem = lexem;
+                this.token = token;
+                this.cLine = cLine;
+
+            }
+        }
+
+        public class LexTokList
+        {
+            public LexemToken head;
+            public LexemToken tail;
+            public void Add(string lexem, Token token, int cLine)
+            {
+                LexemToken node = new LexemToken(lexem, token, cLine);
+
+                if (head == null)
+                {
+                    head = node;
+                }
+                else
+                {
+                    tail.Next = node;
+                }
+                tail = node;
+            }
+
+            public void Display()
+            {
+                LexemToken curr = head;
+                while(curr != null)
+                {
+                    Console.Write(curr.lexem + ", ");
+                    Console.Write(curr.token + ", ");
+                    Console.WriteLine(curr.cLine);
+                    Console.WriteLine("---------------------------------------------------");
+                    curr = curr.Next;
+                }
+            }
+
+        }
+        public static LexTokList GetLexem()
+        {
+
+
+            StreamReader f1 = new StreamReader(@"1.txt");
+
+            string text = f1.ReadToEnd();
+
+            States state = States.Start;
+            Token tokn;
+
+            List<string> KeyWords = new List<string>() { "int", "double", "string", "bool", "class",
+                "private", "protected", "public", "abstract", "extern" , "override", "static", "virtual", "volatile"};
+            List<char> Separator = new List<char>() { ';', '{', '}', '(', ')', ',', ':' };
+
+            List<string> listID = new List<string>();
+
+            int countLine = 1;
+
+            bool dotExist = false;
+
+            string buf = "";
+
+            LexTokList mainList = new LexTokList();
+
+            foreach (char cur_char in text)
+            {
+                switch (state)
+                {
+                    case States.Start:
+
+                        if (cur_char == '\n' || cur_char == '\0' || cur_char == ' ' || cur_char == '\t' || cur_char == '\r')
+                        {
+                            if (cur_char == '\n')
+                            {
+                                countLine++;
+                            }
+                            continue;
+                        }
+
+                        else if (char.IsLetter(cur_char))
+                        {
+                            buf += cur_char;
+                            state = States.ID;
+                        }
+
+                        else if (Separator.Contains(cur_char))
+                        {
+                            tokn = Token.Separator;
+                            mainList.Add(cur_char.ToString(), tokn, countLine);
+                        }
+
+                        else
+                        {
+                            buf += cur_char;
+                            state = States.ERROR;
+                        }
+                        break;
+
+                    case States.ID:
+
+                        if (char.IsLetterOrDigit(cur_char) || cur_char == '_' || cur_char == '.')
+                        {
+                            if (cur_char == '.' && !dotExist)
+                            {
+                                dotExist = true;
+                            }
+                            else if (cur_char == '.' && dotExist)
+                            {
+                                state = States.ERROR;
+                            }
+                            buf += cur_char;
+                        }
+                        else
+                        {
+                            if (cur_char == ' ' || cur_char == '\t' || cur_char == '\0' || cur_char == '\r' || cur_char == '\n' || Separator.Contains(cur_char))
+                            {
+                                if (KeyWords.Contains(buf))
+                                {
+                                    tokn = Token.KeyWord;
+                                    mainList.Add(buf, tokn, countLine);
+                                    buf = "";
+                                }
+                                else
+                                {
+                                    if (!listID.Contains(buf))
+                                    {
+                                        listID.Add(buf);
+                                    }
+                                    tokn = Token.ID;
+                                    mainList.Add(buf, tokn, countLine);
+                                    buf = "";
+                                }
+                                if (cur_char == '\n')
+                                {
+                                    countLine++;
+                                }
+                                if (Separator.Contains(cur_char))
+                                {
+                                    tokn = Token.Separator;
+                                    mainList.Add(cur_char.ToString(), tokn, countLine);
+                                }
+                                state = States.Start;
+                            }
+                            else
+                            {
+                                buf += cur_char;
+                                state = States.ERROR;
+                            }
+                        }
+                        break;
+
+                    case States.ERROR:
+                        if (cur_char != ' ' && cur_char != '\t' && cur_char != '\0' && cur_char != '\r' && cur_char != '\n' && !Separator.Contains(cur_char))
+                        {
+                            buf += cur_char;
+                        }
+                        else
+                        {
+                            tokn = Token.ERROR;
+                            mainList.Add(buf, tokn, countLine);
+                            if (cur_char == '\n')
+                            {
+                                countLine++;
+                            }
+                            if (Separator.Contains(cur_char))
+                            {
+                                tokn = Token.Separator;
+                                mainList.Add(cur_char.ToString(), tokn, countLine);
+                            }
+                            buf = "";
+                            state = States.Start;
+                        }
+                        break;
+                }
+            }
+            f1.Close();
+            return mainList;
+        }
+    }
+}
---
+++ Sintax_my_anal/Program.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using lex_analyzer;
+
+namespace Sintax_my_anal
+{
+    class Program
+    {
+        class ParseException : Exception
+        {
+            public int Value { get; }
+            public ParseException(string message, int val)
+                : base(message)
+            {
+                Value = val;
+            }
+        }
+        public class Parser
+        {
+            public Lexems.LexTokList lex;
+            public Lexems.LexemToken curr;
+
+            public Parser(Lexems.LexTokList lex)
+            {
+                this.lex = lex;
+                curr = lex.head;
+            }
+
+            public void ClassDecl()
+            {
+
+                Header();
+                if (String.Equals(curr.lexem, "class"))
+                {
+                   curr = curr.Next;
+                }
+                else 
+                {
+                   throw new ParseException("Error of compilation, ClassDecl(1)!", curr.cLine);
+                }
+
+                if (curr.token == Token.ID) 
+                {
+                   curr = curr.Next;
+                }
+                else
+                {
+                    throw new ParseException("Error of compilation, ClassDecl(2)!", curr.cLine);
+                }
+
+                ClassBaseOpt();
+
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, "{"))
+                    {
+                        curr = curr.Next;
+                    }
+                    else
+                    {
+                        throw new ParseException("Error of compilation, ClassDecl(3)!", curr.cLine);
+                    }
+                }
+
+                ClassItemDecsOpt();
+
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, "}"))
+                    {
+                        curr = curr.Next;
+                    }
+                    else
+                    {
+                        throw new ParseException("Error of compilation, ClassDecl(4)!", curr.cLine);
+                    }
+                }
+                if (curr != null)
+                {
+                    curr = curr.Next;
+                    ClassDecl();
+                }
+            }
+
+            public void Header()
+            {
+                AccessOpt();
+                Modifier();  
+            }
+
+            public void AccessOpt()
+            {
+
+                if (curr.token == Token.KeyWord )
+                {
+                   if (String.Equals(curr.lexem, "private") || String.Equals(curr.lexem, "protected") || String.Equals(curr.lexem, "public"))
+                   {
+                       curr = curr.Next;
+                   }
+                }
+            }
+
+            public void Modifier()
+            {
+                if (curr.token == Token.KeyWord)
+                { 
+                    if (String.Equals(curr.lexem, "abstract") || String.Equals(curr.lexem, "override") ||
+                        String.Equals(curr.lexem, "static") || String.Equals(curr.lexem, "extern") ||
+                        String.Equals(curr.lexem, "virtual") || String.Equals(curr.lexem, "volatile"))
+                    {
+                        curr = curr.Next;
+                    } 
+                }
+            }
+
+            public void ClassBaseOpt()
+            {
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, ":"))
+                    {
+                        curr = curr.Next;
+                        if (curr.token == Token.ID)
+                        {
+                            curr = curr.Next;
+                        }
+                        else
+                        {
+                            throw new ParseException("Error of compilation, ClassBaseOpt()!", curr.cLine);
+                        }
+                    }
+                }
+            }
+
+            public void ClassItemDecsOpt()
+            {
+                if (curr.lexem != "}")
+                {
+                    ClassItem();
+                }
+            }
+
+            public void ClassItem()
+            {
+                HTID();
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, ";"))
+                    {
+                        curr = curr.Next;
+                        ClassItemDecsOpt();
+                    }
+                    else if (String.Equals(curr.lexem, "("))
+                    {
+                        curr = curr.Next;
+                        MethodDec();
+                    }
+                    else
+                    {
+                        throw new ParseException("Error of compilation, ClassItem()!", curr.cLine);
+                    }
+                }
+            }
+            
+            public void HTID()
+            {
+                Header();
+
+                IdType();
+
+                if (curr.token == Token.ID)
+                {
+                    curr = curr.Next;
+                }
+                else
+                {
+                    throw new ParseException("Error of compilation, HTID()!", curr.cLine);
+                }
+            }
+
+            public void MethodDec()
+            {
+
+                FormalParamListOpt();
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, ")"))
+                    {
+                        curr = curr.Next;
+                    }
+                }
+                else
+                {
+                    throw new ParseException("Error of compilation, MethodDec(1)!", curr.cLine);
+                }
+
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, ";"))
+                    {
+                        curr = curr.Next;
+                    }
+                }
+                else
+                {
+                    throw new ParseException("Error of compilation, MethodDec(2)!", curr.cLine);
+                }
+            }
+
+            public void IdType()
+            {
+                if (curr.token == Token.ID)
+                {
+                    curr = curr.Next;
+                }
+                else if (curr.token == Token.KeyWord)
+                {
+                    if (String.Equals(curr.lexem, "int") || String.Equals(curr.lexem, "double") ||
+                       String.Equals(curr.lexem, "string") || String.Equals(curr.lexem, "bool"))
+                    {
+                        curr = curr.Next;
+                    }
+                }
+                else
+                {
+                    throw new ParseException("Error of compilation, IdType()!", curr.cLine);
+                }
+            }
+            public void FormalParamListOpt()
+            {
+                ParamList();
+            }
+
+            public void ParamList()
+            {
+                IdType();
+                if (curr.token == Token.ID)
+                {
+                    curr = curr.Next;
+                }
+                if (curr.token == Token.Separator)
+                {
+                    if (String.Equals(curr.lexem, ","))
+                    {
+                        curr = curr.Next;
+                        ParamList();
+                    }
+                }
+            } 
+        }
+
+        static void Main()
+        {
+            Lexems.LexTokList lexMas = new Lexems.LexTokList();
+
+            lexMas = Lexems.GetLexem();
+
+            lexMas.Display();
+            Console.WriteLine();
+            Parser p = new Parser(lexMas);
+
+            try
+            {
+                p.ClassDecl();
+                Console.WriteLine("Compilation complete success.");
+            }
+            catch (ParseException e)
+            {
+                Console.WriteLine(e.Message + " CountLine: " + e.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
